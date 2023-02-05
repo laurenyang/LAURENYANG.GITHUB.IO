ac4f36b0bbe866825834bca5ff42f5a03a397691
Lauren Yang <laurenyang@Laurens-Air.attlocal.net>
2023-02-04 22:24:23 -0800

    close to working

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function loadJSON(filePath) {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', filePath, false);
@@ -15,29 +16,82 @@ function loadJSON(filePath) {
 
 var data = loadJSON('data.json');
 console.log(data);
-  
-  
 
-  function generatePreviewCard(bookData) {
-    //let bookCoverImage = bookData.coverImage;
-    let bookAuthor = bookData.author;
-    let bookName = bookData.book_name;
-    let bookCategories = bookData.categories;
-    let bookRating = bookData.rating;
-    let bookRatingText = bookData.rating_text;
-    let dateRead = bookData.date_read;
-  
-    let cardHTML = '<div class="preview-card">' +
-      //'<img src="' + bookCoverImage + '" class="book-cover-image">' +
-      '<div class="book-details">' +
-      '<p class="book-author">Author: ' + bookAuthor + '</p>' +
-      '<p class="book-subject">Subject: ' + bookSubject + '</p>' +
-      '<p class="book-rating">Rating: ' + bookRating + ' stars</p>' +
-      '<p class="date-read">Date Read: ' + dateRead + '</p>' +
-      '</div>' +
-      '<button class="read-more-button">Read More</button>' +
-      '</div>';
-  
-    return cardHTML;
+//TODO: change data to data
+
+/*
+var bookData = [
+  {
+      "book_name": "Upgrade",
+      "author": "Blake Crouch",
+      "categories": [
+          "Sci-Fi",
+          "Fiction"
+      ],
+      "date_read": "2022-11-06T00:00:00.000Z",
+      "date_read_str": "11/6/22",
+      "rating": "4",
+      "rating_text": "☆☆☆☆",
+      "book_image": "images/upgrade.jpg",
+      "preview": "Here’s a hot take: reading > TV. *Upgrade* reminded me how addicting good sci-fi could be."
+  },
+  {
+      "book_name": "Numbers Don't Lie",
+      "author": "Vaclav Smil",
+      "categories": [
+        "Science",
+        "Non-fiction"
+      ],
+      "date_read_str": "8/25/22",
+      "date_read": "2022-08-25T00:00:00.000Z",
+      "rating": "3",
+      "rating_text": "☆☆☆",
+      "book_image": "images/numbersdontlie.jpg",
+      "preview": "Numbers Don’t Lie reads like a fizzy soda on a hot summer day - it’s light, bubbly, refreshing, and…"
   }
-  
+]
+*/
+function generatePreviewCards() {
+  var allCards;
+  for (let i = 0; i < Object.keys(bookData).length; i++) {
+    let book = bookData[i];
+    let cardHTML = generatePreviewCard(book);
+    console.log(cardHTML);
+    allCards = allCards + cardHTML;
+  }
+  allCards = allCards.replace("undefined", "");
+  return allCards;
+}
+
+// book is single book data  
+function generatePreviewCard(book) {
+
+  let cardHTML = '<div class="window">' +
+  '     <div class="title-bar">' +
+  '       <div class="title-bar-text">'+book.book_name+'</div>' +
+  '     </div>' +
+  '     <div class="window-body">' +
+  '         <div style="display: flex; justify-content: center;">' +
+  '             <img style="width:100px; " src=' + book.book_image + ' alt="numbersdontlie cover">' +
+  '         </div>' +
+  '         <fieldset>' +
+  '             <legend>Preview</legend>' +
+  '             <p>'+book.preview+'</p>' +
+  '         </fieldset>' +
+  '     </div>' +
+  '     <div class="window-body" style=" display: flex; justify-content: space-between; "> ' +
+  '         <div style="line-height: 0;">' +
+  '             <p> Rating: '+book.rating_text+'</p>' +
+  '         </div>' +
+  '         <div align="right">' +
+  '             <button >Read more</button> <!-- TODO: make button live on own row -->' +
+  '         </div>' +
+  '     </div>' +
+  '     <div class="status-bar">' +
+  '       <p class="status-bar-field">'+book.categories[0]+'</p>' +
+  '       <p class="status-bar-field">'+book.author+'</p>' +
+  '       <p class="status-bar-field">Read '+book.date_read_str+'</p>' +
+  '     </div>' +
+  ' </div>'
+  return cardHTML;
+}
